a4a26cfceed2aec00c646114632b235aaf841b24
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-08-31 14:50:37 +0530

    created login and homepage

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+
+function Home() {
+
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
+      <h1>VoteMyCourse.com</h1>
+    </div>
+  );
+}
+
+export default Home;
