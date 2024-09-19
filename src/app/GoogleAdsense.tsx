0867437a9efcc28ef41ce899ee101af9b72b81a0
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-09-20 01:42:09 +0530

    oh lord I was implementing richads instead of carbon ads

--- src/app/GoogleAdsense.tsx
+++ src/app/GoogleAdsense.tsx
@@ -5,24 +5,16 @@ type Props = {
 };
 
 const GoogleAdsense: React.FC<Props> = ({ pId }) => {
-  // if (process.env.NODE_ENV !== "production") {
-  //   return null;
-  // }
+  if (process.env.NODE_ENV !== "production") {
+    return null;
+  }
   return (
-    // <Script
-    //   async
-    //   src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
-    //   crossOrigin="anonymous"
-    //   strategy="afterInteractive"
-    // />
     <Script
-    type="module"
-    src="https://richinfo.co/richpartners/push/js/rp-cl-ob.js?pubid=940411&siteid=351360&niche=33"
-    async
-    data-cfasync="false"
-    crossOrigin="anonymous"
-    strategy="lazyOnload"
-  />
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
   );
 };
 
