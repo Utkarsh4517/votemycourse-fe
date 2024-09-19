b49d86f457beddaa5a15a5c88becbe483cacc4f4
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-09-20 00:08:28 +0530

    fixed firebase script not loading up

--- next.config.js
+++ next.config.js
@@ -14,12 +14,8 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://richinfo.co https://cdn.carbonads.com; connect-src 'self' https://richinfo.co https://cdn.carbonads.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline';"
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*'
-          },
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://richinfo.co https://cdn.carbonads.com https://pagead2.googlesyndication.com https://accounts.google.com https://www.gstatic.com; connect-src 'self' https://richinfo.co https://cdn.carbonads.com https://*.googleapis.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline';"
+          }
         ],
       },
     ];
