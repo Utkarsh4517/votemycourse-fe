73bfe646e09da2493695c1aab44faa6d88399102
Utkarsh4517 <uttkarsh.dav@gmail.com>
2024-09-20 00:33:29 +0530

    fixed frame-src error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -14,10 +14,12 @@ module.exports = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://richinfo.co https://cdn.carbonads.com https://pagead2.googlesyndication.com https://accounts.google.com https://www.gstatic.com; connect-src 'self' https://richinfo.co https://cdn.carbonads.com https://*.googleapis.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline';"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://richinfo.co https://cdn.carbonads.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://accounts.google.com https://www.gstatic.com; connect-src 'self' https://richinfo.co https://cdn.carbonads.com https://*.googleapis.com https://pagead2.googlesyndication.com; img-src 'self' https: data:; style-src 'self' 'unsafe-inline'; frame-src 'self' https://googleads.g.doubleclick.net https://accounts.google.com;"
           }
         ],
       },
     ];
   },
 };
+
+module.exports = nextConfig;
